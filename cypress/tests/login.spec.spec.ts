0123bfeff3d5b71ddc105800ece9796a49d50d9b
Marcelo Da Silveira Gomes <marcelogomes1886@gmail.com>
2026-08-27 20:22:43 -0300

    selector objects added

--- cypress/tests/login.spec.spec.ts
+++ cypress/tests/login.spec.spec.ts
@@ -1,18 +1,28 @@
-describe('Login com sucesso', () => {
+describe('Login Tests', () => {
+
+  const selectorsList = {
+    usernameField: "[data-test='signin-username']",
+    passwordField: "[data-test='signin-password']",
+    loginButton: "[data-test='signin-submit']",
+    tabList: "[data-test='nav-transaction-tabs']",
+    wrongCredentialAlert: "[role='alert']",
+  }
+
   it('Deve fazer login com usuário válido', () => {
     cy.visit('/signin')
-    cy.get('#username').type('testuser@example.com')
-    cy.get('#password').type('Test@123456')
-    cy.get('[data-test="signin-submit"]').click()
-    cy.url().should('include', "http://localhost:3000/")
+    cy.get(selectorsList.usernameField).type('testuser@example.com')
+    cy.get(selectorsList.passwordField).type('Test@123456')
+    cy.get(selectorsList.loginButton).click()
+    cy.url().should('include', 'localhost:3000')
+    cy.get(selectorsList.tabList).should('be.visible')
   })
 
-  it('Tentar fazer login com credenciais inválidas', () => {
+  it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
     cy.visit('/signin')
-    cy.get('#username').type('usuarioinvalido@example.com')
-    cy.get('#password').type('senhaerrada123')
-    cy.get('[data-test="signin-submit"]').click()
-    cy.get("[role='alert']").should('be.visible')
-})
+    cy.get(selectorsList.usernameField).type('usuarioinvalido@example.com')
+    cy.get(selectorsList.passwordField).type('senhaerrada123')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert).should('be.visible')
+  })
 
 })
